132aa9ea973374b907fd8416eff70841131d46e5
Roberth Melendez <alexanderrms33@gmail.com>
2025-08-30 16:29:49 -0600

    ola

--- Views/MainForm.Designer.cs
+++ Views/MainForm.Designer.cs
@@ -20,140 +20,145 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-			menuStrip1 = new MenuStrip();
-			cursosToolStripMenuItem = new ToolStripMenuItem();
-			nuevoCursoToolStripMenuItem = new ToolStripMenuItem();
-			nuevoCursoToolStripMenuItem1 = new ToolStripMenuItem();
-			alumnosToolStripMenuItem = new ToolStripMenuItem();
-			alumnosActualesToolStripMenuItem = new ToolStripMenuItem();
-			nuevoAlumnoToolStripMenuItem = new ToolStripMenuItem();
-			inscripcionesToolStripMenuItem = new ToolStripMenuItem();
-			inscribirseAAlumnoToolStripMenuItem = new ToolStripMenuItem();
-			sesionesToolStripMenuItem = new ToolStripMenuItem();
-			docentesActualesToolStripMenuItem = new ToolStripMenuItem();
-			nuevoDocenteToolStripMenuItem = new ToolStripMenuItem();
-			cursosPorDocenteToolStripMenuItem = new ToolStripMenuItem();
-			menuStrip1.SuspendLayout();
-			SuspendLayout();
-			// 
-			// menuStrip1
-			// 
-			menuStrip1.ImageScalingSize = new Size(20, 20);
-			menuStrip1.Items.AddRange(new ToolStripItem[] { cursosToolStripMenuItem, alumnosToolStripMenuItem, inscripcionesToolStripMenuItem, sesionesToolStripMenuItem });
-			menuStrip1.Location = new Point(0, 0);
-			menuStrip1.Name = "menuStrip1";
-			menuStrip1.Size = new Size(1063, 28);
-			menuStrip1.TabIndex = 1;
-			menuStrip1.Text = "menuStrip1";
-			// 
-			// cursosToolStripMenuItem
-			// 
-			cursosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nuevoCursoToolStripMenuItem, nuevoCursoToolStripMenuItem1 });
-			cursosToolStripMenuItem.Name = "cursosToolStripMenuItem";
-			cursosToolStripMenuItem.Size = new Size(66, 24);
-			cursosToolStripMenuItem.Text = "Cursos";
-			// 
-			// nuevoCursoToolStripMenuItem
-			// 
-			nuevoCursoToolStripMenuItem.Name = "nuevoCursoToolStripMenuItem";
-			nuevoCursoToolStripMenuItem.Size = new Size(224, 26);
-			nuevoCursoToolStripMenuItem.Text = "Cursos Actuales";
-			// 
-			// nuevoCursoToolStripMenuItem1
-			// 
-			nuevoCursoToolStripMenuItem1.Name = "nuevoCursoToolStripMenuItem1";
-			nuevoCursoToolStripMenuItem1.Size = new Size(224, 26);
-			nuevoCursoToolStripMenuItem1.Text = "Nuevo Curso";
-			nuevoCursoToolStripMenuItem1.Click += nuevoCursoToolStripMenuItem1_Click;
-			// 
-			// alumnosToolStripMenuItem
-			// 
-			alumnosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { alumnosActualesToolStripMenuItem, nuevoAlumnoToolStripMenuItem });
-			alumnosToolStripMenuItem.Name = "alumnosToolStripMenuItem";
-			alumnosToolStripMenuItem.Size = new Size(81, 24);
-			alumnosToolStripMenuItem.Text = "Alumnos";
-			// 
-			// alumnosActualesToolStripMenuItem
-			// 
-			alumnosActualesToolStripMenuItem.Name = "alumnosActualesToolStripMenuItem";
-			alumnosActualesToolStripMenuItem.Size = new Size(210, 26);
-			alumnosActualesToolStripMenuItem.Text = "Alumnos Actuales";
-			// 
-			// nuevoAlumnoToolStripMenuItem
-			// 
-			nuevoAlumnoToolStripMenuItem.Name = "nuevoAlumnoToolStripMenuItem";
-			nuevoAlumnoToolStripMenuItem.Size = new Size(210, 26);
-			nuevoAlumnoToolStripMenuItem.Text = "Nuevo Alumno";
-			// 
-			// inscripcionesToolStripMenuItem
-			// 
-			inscripcionesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { inscribirseAAlumnoToolStripMenuItem });
-			inscripcionesToolStripMenuItem.Name = "inscripcionesToolStripMenuItem";
-			inscripcionesToolStripMenuItem.Size = new Size(108, 24);
-			inscripcionesToolStripMenuItem.Text = "Inscripciones";
-			// 
-			// inscribirseAAlumnoToolStripMenuItem
-			// 
-			inscribirseAAlumnoToolStripMenuItem.Name = "inscribirseAAlumnoToolStripMenuItem";
-			inscribirseAAlumnoToolStripMenuItem.Size = new Size(226, 26);
-			inscribirseAAlumnoToolStripMenuItem.Text = "Inscribirse a Alumno";
-			// 
-			// sesionesToolStripMenuItem
-			// 
-			sesionesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { docentesActualesToolStripMenuItem, nuevoDocenteToolStripMenuItem, cursosPorDocenteToolStripMenuItem });
-			sesionesToolStripMenuItem.Name = "sesionesToolStripMenuItem";
-			sesionesToolStripMenuItem.Size = new Size(85, 24);
-			sesionesToolStripMenuItem.Text = "Docentes";
-			// 
-			// docentesActualesToolStripMenuItem
-			// 
-			docentesActualesToolStripMenuItem.Name = "docentesActualesToolStripMenuItem";
-			docentesActualesToolStripMenuItem.Size = new Size(222, 26);
-			docentesActualesToolStripMenuItem.Text = "Docentes Actuales";
-			// 
-			// nuevoDocenteToolStripMenuItem
-			// 
-			nuevoDocenteToolStripMenuItem.Name = "nuevoDocenteToolStripMenuItem";
-			nuevoDocenteToolStripMenuItem.Size = new Size(222, 26);
-			nuevoDocenteToolStripMenuItem.Text = "Nuevo Docente";
-			// 
-			// cursosPorDocenteToolStripMenuItem
-			// 
-			cursosPorDocenteToolStripMenuItem.Name = "cursosPorDocenteToolStripMenuItem";
-			cursosPorDocenteToolStripMenuItem.Size = new Size(222, 26);
-			cursosPorDocenteToolStripMenuItem.Text = "Cursos por Docente";
-			// 
-			// MainForm
-			// 
-			AutoScaleDimensions = new SizeF(8F, 20F);
-			AutoScaleMode = AutoScaleMode.Font;
-			BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-			BackgroundImageLayout = ImageLayout.Stretch;
-			ClientSize = new Size(1063, 450);
-			Controls.Add(menuStrip1);
-			Icon = (Icon)resources.GetObject("$this.Icon");
-			IsMdiContainer = true;
-			MainMenuStrip = menuStrip1;
-			Name = "MainForm";
-			Text = "Menú";
-			menuStrip1.ResumeLayout(false);
-			menuStrip1.PerformLayout();
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            menuStrip1 = new MenuStrip();
+            cursosToolStripMenuItem = new ToolStripMenuItem();
+            nuevoCursoToolStripMenuItem = new ToolStripMenuItem();
+            nuevoCursoToolStripMenuItem1 = new ToolStripMenuItem();
+            alumnosToolStripMenuItem = new ToolStripMenuItem();
+            alumnosActualesToolStripMenuItem = new ToolStripMenuItem();
+            nuevoAlumnoToolStripMenuItem = new ToolStripMenuItem();
+            inscripcionesToolStripMenuItem = new ToolStripMenuItem();
+            inscribirseAAlumnoToolStripMenuItem = new ToolStripMenuItem();
+            sesionesToolStripMenuItem = new ToolStripMenuItem();
+            docentesActualesToolStripMenuItem = new ToolStripMenuItem();
+            nuevoDocenteToolStripMenuItem = new ToolStripMenuItem();
+            cursosPorDocenteToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cursosToolStripMenuItem, alumnosToolStripMenuItem, inscripcionesToolStripMenuItem, sesionesToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(9, 3, 0, 3);
+            menuStrip1.Size = new Size(1594, 40);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cursosToolStripMenuItem
+            // 
+            cursosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nuevoCursoToolStripMenuItem, nuevoCursoToolStripMenuItem1 });
+            cursosToolStripMenuItem.Name = "cursosToolStripMenuItem";
+            cursosToolStripMenuItem.Size = new Size(93, 34);
+            cursosToolStripMenuItem.Text = "Cursos";
+            // 
+            // nuevoCursoToolStripMenuItem
+            // 
+            nuevoCursoToolStripMenuItem.Name = "nuevoCursoToolStripMenuItem";
+            nuevoCursoToolStripMenuItem.Size = new Size(315, 40);
+            nuevoCursoToolStripMenuItem.Text = "Cursos Actuales";
+            nuevoCursoToolStripMenuItem.Click += nuevoCursoToolStripMenuItem_Click_1;
+            // 
+            // nuevoCursoToolStripMenuItem1
+            // 
+            nuevoCursoToolStripMenuItem1.Name = "nuevoCursoToolStripMenuItem1";
+            nuevoCursoToolStripMenuItem1.Size = new Size(315, 40);
+            nuevoCursoToolStripMenuItem1.Text = "Nuevo Curso";
+            nuevoCursoToolStripMenuItem1.Click += nuevoCursoToolStripMenuItem1_Click;
+            // 
+            // alumnosToolStripMenuItem
+            // 
+            alumnosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { alumnosActualesToolStripMenuItem, nuevoAlumnoToolStripMenuItem });
+            alumnosToolStripMenuItem.Name = "alumnosToolStripMenuItem";
+            alumnosToolStripMenuItem.Size = new Size(113, 34);
+            alumnosToolStripMenuItem.Text = "Alumnos";
+            // 
+            // alumnosActualesToolStripMenuItem
+            // 
+            alumnosActualesToolStripMenuItem.Name = "alumnosActualesToolStripMenuItem";
+            alumnosActualesToolStripMenuItem.Size = new Size(298, 40);
+            alumnosActualesToolStripMenuItem.Text = "Alumnos Actuales";
+            // 
+            // nuevoAlumnoToolStripMenuItem
+            // 
+            nuevoAlumnoToolStripMenuItem.Name = "nuevoAlumnoToolStripMenuItem";
+            nuevoAlumnoToolStripMenuItem.Size = new Size(298, 40);
+            nuevoAlumnoToolStripMenuItem.Text = "Nuevo Alumno";
+            // 
+            // inscripcionesToolStripMenuItem
+            // 
+            inscripcionesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { inscribirseAAlumnoToolStripMenuItem });
+            inscripcionesToolStripMenuItem.Name = "inscripcionesToolStripMenuItem";
+            inscripcionesToolStripMenuItem.Size = new Size(151, 34);
+            inscripcionesToolStripMenuItem.Text = "Inscripciones";
+            // 
+            // inscribirseAAlumnoToolStripMenuItem
+            // 
+            inscribirseAAlumnoToolStripMenuItem.Name = "inscribirseAAlumnoToolStripMenuItem";
+            inscribirseAAlumnoToolStripMenuItem.Size = new Size(320, 40);
+            inscribirseAAlumnoToolStripMenuItem.Text = "Inscribirse a Alumno";
+            // 
+            // sesionesToolStripMenuItem
+            // 
+            sesionesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { docentesActualesToolStripMenuItem, nuevoDocenteToolStripMenuItem, cursosPorDocenteToolStripMenuItem });
+            sesionesToolStripMenuItem.Name = "sesionesToolStripMenuItem";
+            sesionesToolStripMenuItem.Size = new Size(118, 34);
+            sesionesToolStripMenuItem.Text = "Docentes";
+            // 
+            // docentesActualesToolStripMenuItem
+            // 
+            docentesActualesToolStripMenuItem.Name = "docentesActualesToolStripMenuItem";
+            docentesActualesToolStripMenuItem.Size = new Size(315, 40);
+            docentesActualesToolStripMenuItem.Text = "Docentes Actuales";
+            // 
+            // nuevoDocenteToolStripMenuItem
+            // 
+            nuevoDocenteToolStripMenuItem.Name = "nuevoDocenteToolStripMenuItem";
+            nuevoDocenteToolStripMenuItem.Size = new Size(315, 40);
+            nuevoDocenteToolStripMenuItem.Text = "Nuevo Docente";
+            nuevoDocenteToolStripMenuItem.Click += nuevoDocenteToolStripMenuItem_Click;
+            // 
+            // cursosPorDocenteToolStripMenuItem
+            // 
+            cursosPorDocenteToolStripMenuItem.Name = "cursosPorDocenteToolStripMenuItem";
+            cursosPorDocenteToolStripMenuItem.Size = new Size(315, 40);
+            cursosPorDocenteToolStripMenuItem.Text = "Cursos por Docente";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1594, 675);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4);
+            Name = "MainForm";
+            Text = "Menú";
+            Load += MainForm_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private MenuStrip menuStrip1;
+        private MenuStrip menuStrip1;
 		private ToolStripMenuItem cursosToolStripMenuItem;
 		private ToolStripMenuItem alumnosToolStripMenuItem;
 		private ToolStripMenuItem inscripcionesToolStripMenuItem;
--- Views/MainForm.cs
+++ Views/MainForm.cs
@@ -2,21 +2,36 @@ using CursosLibres.Views;
 
 namespace CursosLibres
 {
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			InitializeComponent();
-		}
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
 
-		private void nuevoCursoToolStripMenuItem_Click(object sender, EventArgs e)
-		{
+        private void nuevoCursoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void nuevoCursoToolStripMenuItem1_Click(object sender, EventArgs e)
-		{
-			new FrmCursos { MdiParent = this }.Show();
-		}
-	}
+        private void nuevoCursoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            new FrmCursos { MdiParent = this }.Show();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nuevoCursoToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nuevoDocenteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*/*new FrmDocente { MdiParent = this }.Show();*/
+        }
+    }
 }
